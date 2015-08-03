a98dd31c84a0c731998c7945ae5c4f0f3a23cf53
AjitSingh <jeetsingh.ajit@gmail.com>
2015-08-04 00:09:06 +0530

    adding yellow color to the text

--- print_random_message.sh
+++ print_random_message.sh
@@ -3,4 +3,7 @@ total_messages=$(cat $MESSAGES | wc -l)
 random_index=$[RANDOM % $total_messages + 1]
 
 random_message=$(head -$random_index $MESSAGES | tail -1)
-echo $random_message
+
+YELLOW='\033[0;33m'
+NoColor='\033[0m'
+printf "${YELLOW}$random_message${NoColor}\n"
